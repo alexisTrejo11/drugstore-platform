42ccac97278e121ac98275fc753c0d432777db9d
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-10-30 17:50:12 -0600

    chore: Update gitignore and remove sensitive files

--- config-server/src/main/java/microservice/config_service/ConfigServerApplication.java
+++ config-server/src/main/java/microservice/config_service/ConfigServerApplication.java
@@ -1,16 +1,29 @@
 package microservice.config_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
 
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+            System.out.println("Loaded env: " + entry.getKey() + "=***"); // Mask secrets
+        });
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
